e3210fd05ddd3acc8ae30a72fff9c222670c9e73
divyanshi-sharma <57505733+divyanshi-sharma@users.noreply.github.com>
2020-08-04 20:37:31 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
@@ -13,6 +15,10 @@ app.get('/users', function(req,res){
         })
 })
 
+
+/*In Express, words with a colon in front of them in the url are treated as variables. 
+You can access the value of each variable through req.params, like this: */
+// colons are used as variables that be viewed in the params
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 	res.json({
@@ -22,6 +28,27 @@ app.get('/users/:id',function(req,res){
 	})
 })
 
-app.listen(8000,function(){
-console.log("server is running")
+//a function to handle a POST request made to the 'login' endpoint
+
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
 })
+
+app.listen(8000,function(){console.log("server is running")})
